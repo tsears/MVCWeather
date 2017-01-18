8c6961c577fa4c0a90287f63fe7741a0eb5d2c12
Tom Sears <tom@tsears.net>
2017-01-18 08:03:55 -0800

    Loading Spinner
    Added loading spinner while query is being performed.

--- src/MVCWeather/Frontend/jsapp/WeatherController.js
+++ src/MVCWeather/Frontend/jsapp/WeatherController.js
@@ -1,21 +1,26 @@
 export default class WeatherController {
-    constructor($http, $timeout, $cookies) {
+    constructor($http, $timeout, $cookies, iconMappings) {
         this.http = $http;
         this.timeout = $timeout;
         this.cookies = $cookies;
         this.query = '';
         this.visibility = 'hidden';
+        this.loading = 'false';
+        this.iconMappings = iconMappings;
 
         const lastQuery = this.cookies.get('lastQuery');
         if (lastQuery) {
+            this.loading = true;
             this.query = lastQuery;
             this.getWeatherData();
+            this.visibility = 'visible';
         }
     }
 
     getWeatherData() {
         const self = this;
         const url = `/api/Weather?query=${this.query}`;
+        this.loading = true;
         
         self.http.get(url).then((resp) => {
             this.cookies.put('lastQuery', this.query);
@@ -25,7 +30,10 @@ export default class WeatherController {
             self.alerts = resp.data.alerts;
 
             this.timeout(() => {
-                this.visibility = 'visible';
+                if (this.visibility === 'hidden') {
+                    this.visibility = 'visible';
+                }
+                this.loading = false;
             }, 1000); 
         });
     }
--- src/MVCWeather/Frontend/jsapp/weather.js
+++ src/MVCWeather/Frontend/jsapp/weather.js
@@ -10,19 +10,20 @@ d.directive('forecastDayCard', () => new ForecastDay());
 d.directive('alert', () => new Alert());
 
 d.constant('iconMappings', {
-    'rain' : '/icons/rain.svg', //
-    'clear-day' : '/icons/clear-day.svg', //
-    'clear-night' : '/icons/clear-night.svg', //
-    'snow' : '/icons/snow.svg', //
-    'sleet' : '/icons/sleet.svg', //
-    'wind' : '/icons/wind.svg', //
-    'fog' : '/icons/fog.svg', //
-    'cloudy' : '/icons/cloudy.svg', //
-    'partly-cloudy-day' : '/icons/partly-cloudy-day.svg', //
-    'partly-cloudy-night' : '/icons/partly-cloudy-night.svg', //
-    'default': '/icons/default.svg'
+    'rain' : '/icons/rain.svg',
+    'clear-day' : '/icons/clear-day.svg',
+    'clear-night' : '/icons/clear-night.svg',
+    'snow' : '/icons/snow.svg',
+    'sleet' : '/icons/sleet.svg',
+    'wind' : '/icons/wind.svg',
+    'fog' : '/icons/fog.svg',
+    'cloudy' : '/icons/cloudy.svg',
+    'partly-cloudy-day' : '/icons/partly-cloudy-day.svg',
+    'partly-cloudy-night' : '/icons/partly-cloudy-night.svg',
+    'default': '/icons/default.svg',
+    'loading': '/icons/loading.svg',
 });
 
 const app = angular.module('weatherApp', ['weatherDirectives', 'ngAnimate', 'ngCookies']);
 
-app.controller('weatherController', ['$http', '$timeout', '$cookies', WeatherController]);
+app.controller('weatherController', ['$http', '$timeout', '$cookies', 'iconMappings', WeatherController]);
