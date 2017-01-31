419b992672e980821a220cb881d98a2e4bcc711b
Tom Sears <tom@tsears.net>
2017-01-30 19:26:04 -0800

    Unit Test Updates
    Added mocks for Memcached client.

--- test/MVCWeather.Tests/DarkskyQueryServiceTests.cs
+++ test/MVCWeather.Tests/DarkskyQueryServiceTests.cs
@@ -4,6 +4,9 @@ using Moq;
 using tsears.MVCWeather.Services.Weather;
 using System.Collections.Generic;
 using tsears.MVCWeather.DataStructures;
+using Enyim.Caching;
+using Enyim.Caching.Memcached.Results;
+using System.Threading.Tasks;
 
 
 namespace tsears.MVCWeather.Services.Weather.Tests
@@ -12,15 +15,19 @@ namespace tsears.MVCWeather.Services.Weather.Tests
     {
         private IWeatherQueryService _darkskyQueryService;
 
-        public DarkskyQueryServiceTests() {            
+        public DarkskyQueryServiceTests() {
             var mockQueryDispatchSvc = new Mock<IWeatherQueryDispatchService>();
             mockQueryDispatchSvc.Setup(q => q.Query(It.IsAny<string>())).ReturnsAsync(new ForecastResponse());
 
-            _darkskyQueryService = new DarkskyQueryService(mockQueryDispatchSvc.Object);
+            var mockMemcachedClient = new Mock<IMemcachedClient>();
+            var mockOpResult = new Mock<IGetOperationResult<ForecastResponse>>();
+            mockMemcachedClient.Setup(m => m.GetAsync<ForecastResponse>(It.IsAny<string>())).Returns(Task.FromResult(mockOpResult.Object));
+
+            _darkskyQueryService = new DarkskyQueryService(mockQueryDispatchSvc.Object, mockMemcachedClient.Object);
         }
 
         [Fact]
-        public async void ReturnsForecast() 
+        public async void ReturnsForecast()
         {
             var result = await _darkskyQueryService.Query(new GeoCoordinate("0", "0"));
 
--- test/MVCWeather.Tests/GeocodioQueryServiceTests.cs
+++ test/MVCWeather.Tests/GeocodioQueryServiceTests.cs
@@ -1,6 +1,9 @@
 ﻿using Xunit;
 using Moq;
 using System.Collections.Generic;
+using Enyim.Caching;
+using Enyim.Caching.Memcached.Results;
+using System.Threading.Tasks;
 
 namespace tsears.MVCWeather.Services.Geo.Tests
 {
@@ -11,15 +14,19 @@ namespace tsears.MVCWeather.Services.Geo.Tests
         public GeocodioQueryServiceTests() {
             var mockParser = new Mock<IGeoQueryParser>();
             mockParser.Setup(p => p.Parse(It.IsAny<string>())).Returns(new Dictionary<string, string>() { {"postal_code", "12345"} });
-            
+
             var mockQueryDispatchSvc = new Mock<IGeoQueryDispatchService>();
             mockQueryDispatchSvc.Setup(q => q.Query(It.IsAny<string>())).ReturnsAsync(new GeoResponse());
 
-            _geocodioQueryService = new GeocodioQueryService(mockParser.Object, mockQueryDispatchSvc.Object);
+            var mockMemcachedClient = new Mock<IMemcachedClient>();
+            var mockOpResult = new Mock<IGetOperationResult<GeoResponse>>();
+            mockMemcachedClient.Setup(m => m.GetAsync<GeoResponse>(It.IsAny<string>())).Returns(Task.FromResult(mockOpResult.Object));
+
+            _geocodioQueryService = new GeocodioQueryService(mockParser.Object, mockQueryDispatchSvc.Object, mockMemcachedClient.Object);
         }
 
         [Fact]
-        public async void ReturnsGeoInfo() 
+        public async void ReturnsGeoInfo()
         {
             var result = await _geocodioQueryService.Query("foo");
 
